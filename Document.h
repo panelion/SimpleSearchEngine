a9ce063bee755281f4b116c730f638d311b15108
David Woo <panelion@gmail.com>
2016-02-26 15:32:49 +0900

    Add Comments from Document.h and Indexer.h

--- Document.h
+++ Document.h
@@ -11,8 +11,52 @@
 #include "utils.h"
 
 
+/******************************************************************************
+ *
+ * - Document
+ *
+ * 문서의 정보를 저장하는 객체
+ *
+ * 정수형으로 이루어진 Document ID 와 원본 내용을 저장한다.
+ * 원본 내용에서 분석된 Term 목록을 가지고 있다.
+ *
+ ******************************************************************************/
 class Document
 {
+
+public:
+    /**
+     * Constructor
+     */
+    Document();
+    Document(uint64_t id, string contents);
+
+    ~Document();
+
+    /**
+     * Document 의 ID 값을 반환한다.
+     *
+     * @return uint64_t Document ID 값
+     */
+    uint64_t getId() const;
+
+    /**
+     * Document 의 내용을 반환한다
+     *
+     * @return string Document 의 내용
+     */
+    std::string getContents() const;
+
+
+    bool getTerm(const std::string& sTerm, Term*& returnTerm);
+
+    /**
+     * Document 의 Contents 로 부터 형태소 분석 된 단어의 목록을 반환한다.
+     *
+     * @return vector<Term> Term 객체의 목록
+     */
+    vector<Term>& getTerms();
+
 protected:
     const std::string SEPARATOR = " ";
     uint64_t mId;
@@ -20,16 +64,6 @@ protected:
     std::vector<Term> mTermsVector;
 
     void createTerm();
-
-public:
-    Document();
-    Document(uint64_t id, string contents);
-    ~Document();
-
-    uint64_t getId() const;
-    std::string getContents() const;
-    bool getTerm(const std::string& sTerm, Term*& returnTerm);
-    vector<Term>& getTerms();
 };
 
 #endif //DOCUMENT_H
--- Indexer.h
+++ Indexer.h
@@ -6,11 +6,44 @@
 #include <vector>
 #include "Document.h"
 
+/******************************************************************************
+ *
+ * - Indexer
+ *
+ * Map<string, vector<uint64_t> 의 형식의 Index Store 자료 구조에
+ * 검색 단어를 Key 로 하고, 해당 단어를 가진 Document 들의 ID 값을 Value 로 저장한다.
+ *
+ * 검색시에는 질의 요청 단어를 Index Store 에 해당 Key 로 가진지 여부를 조회한다.
+ *
+ * 조회 성공시, 해당 Key 가 가진 Value 즉, Document ID 의 목록을 반환한다.
+ *
+ *
+ ******************************************************************************/
 class Indexer {
 
 public:
+    /**
+     * Keyword 를 Key 로 하여, document ID 값을 Indexing 한다
+     *
+     * @param string    Keyword
+     * @param uint64_t  document 의 ID 값
+     */
     void insertKeyword(const std::string &keyword, uint64_t documentId);
+
+    /**
+     * Indexing Data 에 질의한 keyword 를 이용하여 검색한다
+     *
+     * @param string    조회 Keyword
+     *
+     * @return bool     true: 검색 결과 존재, false: 검색 결과 존재 안함
+     */
     bool search(const std::string &keyword);
+
+    /**
+     * 검색의 결과 값을 반환한다
+     *
+     * @return vector<uint64_t> document ids.
+     */
     std::vector<uint64_t> getResult() const;
 
 protected:
